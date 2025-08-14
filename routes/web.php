1f57170349e427a32e13d09d89a49637bc317c88
nekoLin <krusiawanbudi1@gmail.com>
2025-08-14 18:20:30 +0800

    feat: redirect for changed url manually

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+Route::get('/mepandes', fn () => Inertia::render('mepandes/index'))->name('mepandes');
+
+Route::get('/{any}', fn () => to_route('mepandes'))->where('any', '.*');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
